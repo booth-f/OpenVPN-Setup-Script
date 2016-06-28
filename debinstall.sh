ed5f8a9a4a3e8e93b0a23816ba82112eb50aaf82
Francis Booth <rizen@atlum.net>
2016-06-28 00:51:56 -0400

    Specify internet interface for iptables
    Fixed an issue where systems which did not default to using eth0 as their systems default interface can now specify the right one during the installation.

--- debinstall.sh
+++ debinstall.sh
@@ -5,6 +5,9 @@ echo "This script must be run as root."
 exit
 fi
 
+echo -n "Please specify which interface is connected to the Internet (If unsure use eth0): "
+read IFACE
+
 apt-get update
 apt-get install openvpn curl
 cd /etc/openvpn/
@@ -21,7 +24,7 @@ source ./vars
 ./build-dh
 cd keys
 cp dh2048.pem ca.crt server.crt server.key /etc/openvpn
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o "$IFACE" -j MASQUERADE
 echo 1 > /proc/sys/net/ipv4/ip_forward
 service openvpn start
 echo "Installation Complete, your server should be all set up and ready to accept clients."
